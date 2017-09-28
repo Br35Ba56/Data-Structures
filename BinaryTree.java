ab4b3e46feb5762e997d01bb80beafcea37b6f16
Anthony Schneider <tonyschndr@gmail.com>
2017-09-28 18:50:15 +0200

    Working on BinaryTree.

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,17 +1,18 @@
 package datastructures;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTree<T extends Comparable<T>> {
 
-    private Node<T> root;
+    private TreeNode<T> root;
+    private List<TreeNode<T>> treeList = new LinkedList<>();
 
     public BinaryTree() {
 
     }
 
-    public BinaryTree(Node root) {
+    public BinaryTree(TreeNode root) {
         this.root = root;
     }
 
@@ -23,11 +24,29 @@ public class BinaryTree<T extends Comparable<T>> {
 
     }
 
-    public void add(T element) {
-        addNode(root, new Node(element));
+    public static void main(String... args) {
+
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        for (int i = 0; i < 10; i++) {
+            tree.add((int) (Math.random() * 100));
+
+        }
+
+        tree.balanceTree();
+        System.out.println("root: " + tree.root);
+        List<TreeNode<Integer>> listTree = tree.getInOrderList(tree.root);
+
+        for (TreeNode node : listTree) {
+            int i = tree.getDepth(node);
+            System.out.println("depth of " + node.toString() + " is " + i);
+        }
     }
 
-    private void addNode(Node tmpNode, Node newNode) {
+    public void add(T element) {
+        addNode(root, new TreeNode(element));
+    }
+
+    private void addNode(TreeNode tmpNode, TreeNode newNode) {
         if(root == null) {
             root = newNode;
         } else if (newNode.element.compareTo(tmpNode.element) <= 0) {
@@ -37,7 +56,7 @@ public class BinaryTree<T extends Comparable<T>> {
                 tmpNode.leftChild = newNode;
             }
         } else {
-            if (tmpNode.leftChild == newNode) {
+            if (tmpNode.rightChild != null) {
                 addNode(tmpNode.rightChild, newNode);
             } else {
                 tmpNode.rightChild = newNode;
@@ -45,37 +64,65 @@ public class BinaryTree<T extends Comparable<T>> {
         }
     }
 
-    private void balanceTree(Node root) {
-
+    private void balanceTree() {
+        treeList = getInOrderList(root);
+        root = null;
+        int middle = 0;
+        while (treeList.size() != 0) {
+            middle = treeList.size() / 2;
+            TreeNode<T> node = treeList.remove(middle);
+            this.add(node.element);
+        }
     }
 
-    public void inOrderTraverse(Node root) {
-        Node tmpNode = root;
+    public int getDepth(TreeNode root) {
+        if (root == null) return 0;
 
-        if (tmpNode.leftChild != null) {
-            inOrderTraverse(root.leftChild);
+        int leftDepth = getDepth(root.leftChild);
+        int rightDepth = getDepth(root.rightChild);
+
+        if (root.leftChild != null) {
+            return 1 + rightDepth;
         }
+        if (root.rightChild != null) {
+            return 1 + leftDepth;
+        }
+        return 1 + Math.min(leftDepth, rightDepth);
+    }
+
+    public List<TreeNode<T>> getInOrderList(TreeNode root) {
+
+        if (root != null) {
+            getInOrderList(root.leftChild);
+            treeList.add(root);
+            getInOrderList(root.rightChild);
+        }
+        return treeList;
+    }
+
+    public void reverseInOrder(TreeNode root) {
+        TreeNode tmpNode = root;
         if (tmpNode.rightChild != null) {
-            inOrderTraverse(root.rightChild);
+            reverseInOrder(tmpNode.rightChild);
+        }
+        if (tmpNode.leftChild != null) {
+            reverseInOrder(tmpNode.leftChild);
         }
     }
 
-    private class Node<T extends Comparable<T>> {
+    private static class TreeNode<T extends Comparable<T>> {
         T element;
-        Node leftChild;
-        Node rightChild;
+        TreeNode leftChild;
+        TreeNode rightChild;
 
-        public Node(T element) {
+        public TreeNode(T element) {
             this.element = element;
         }
-    }
 
-    public static void main(String... args) {
-       List<Integer> list = new ArrayList<>();
-       for (int i = 0; i < 20; i++) {
-           list.add(i);
-       }
-       BinaryTree<Integer> tree = new BinaryTree<>(list);
-
+        @Override
+        public String toString() {
+            return element.toString();
+        }
     }
 }
+
